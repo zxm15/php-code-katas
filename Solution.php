a9651d52ced2fec0b5ed838806202c6a83f8bdf9
zxm15 <gavingan9e@gmail.com>
2015-09-02 16:33:01 -0400

    Created the first exeception tests

--- Solution.php
+++ Solution.php
@@ -8,6 +8,7 @@ class Solution {
 	public static function findPrimeFactors($value) {
 		//check edge cases
 		if (! is_int($value)) throw new InvalidArgumentException("The input is not an integer");
+		if ($value < 0) throw new InvalidArgumentException("The input cannot be a negtive value");
 		$solution = array();
 		for ($prime = 2; $prime <= $value; $prime++) {
 			while ($value % $prime == 0) {
--- SolutionTest.php
+++ SolutionTest.php
@@ -1,6 +1,7 @@
 <?php 
 
 require_once 'Solution.php';
+
 class SolutionTest extends PHPUnit_Framework_TestCase {
 
 	public function testPrimeFactors() {
@@ -10,5 +11,20 @@ class SolutionTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals([3,3], Solution::findPrimeFactors(9));
 		$this->assertEquals([2,2,5,5], Solution::findPrimeFactors(100));
 		$this->assertEquals([11,11], Solution::findPrimeFactors(121));
+	}
+
+	/**
+     * @expectedException        InvalidArgumentException
+     */
+	public function testExceptions() {
+		$this->assertEquals([], Solution::findPrimeFactors(-1));
+	}
+
+	/**
+     * @expectedException        InvalidArgumentException
+     * @expectedExceptionMessage The input cannot be a negtive value
+     */
+	public function testExceptionsMessages() {
+		$this->assertEquals([], Solution::findPrimeFactors(-100));
 	}
 }
